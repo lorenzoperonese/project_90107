53655c9d87558c9c0877e028c8dbd901e9a2ace0
Lele <emanuele.2003@yahoo.it>
2025-05-23 17:12:07 +0000

    rewrite API endpoint to backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,4 +15,15 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    proxy: {
+      // tutte le chiamate che iniziano con /api saranno inoltrate a localhost:3000
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+        // pathRewrite: { '^/api': '' } // Se il server non ha bisogno di /api
+      }
+    }
+  }
 })
